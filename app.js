c264adb22fa7b6c138926b4ae8c8a801283175d9
Niansyah Eko Putra <61461755+nep0x0@users.noreply.github.com>
2021-09-27 19:18:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -123,17 +123,17 @@ const domElement = document.getElementById('tvchart');
 const chart = LightweightCharts.createChart(domElement,chartProperties);
 const candleSeries = chart.addCandlestickSeries();
 
-// https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=1000
+// // https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=1000
 
-fetch(`https://fapi.binance.com/fapi/v1/klines?symbol=BTCUSDT&interval=1m&limit=1000`)
-  .then(res => res.json())
-  .then(data => {
-    const cdata = data.map(d => {
-      return {time:d[0]/1000,open:parseFloat(d[1]),high:parseFloat(d[2]),low:parseFloat(d[3]),close:parseFloat(d[4])}
-    });
-    candleSeries.setData(cdata);
-  })
-  .catch(err => log(err))
+// fetch(`https://fapi.binance.com/fapi/v1/klines?symbol=BTCUSDT&interval=1m&limit=1000`)
+//   .then(res => res.json())
+//   .then(data => {
+//     const cdata = data.map(d => {
+//       return {time:d[0]/1000,open:parseFloat(d[1]),high:parseFloat(d[2]),low:parseFloat(d[3]),close:parseFloat(d[4])}
+//     });
+//     candleSeries.setData(cdata);
+//   })
+//   .catch(err => log(err))
 
 let fWebSocket = new WebSocket('wss://fstream.binance.com/ws/btcusdt@kline_1m');
 
